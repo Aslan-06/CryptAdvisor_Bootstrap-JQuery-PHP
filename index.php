b4aeac355b8149969a98fddc0aae2064c1581c31
Aslan-06 <83704621+Aslan-06@users.noreply.github.com>
2021-12-02 17:11:50 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Crypt Advisor</title>
-    <link rel="stylesheet" href="assets/css/style.css" />
-</head>
-
-<body>
-    <?php
+<?php
        require_once("header.html");
-    ?>
-</body>
-
-</html>
+?>
