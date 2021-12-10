56a7c83801c5a870b189adbbf14b9a4108217427
busra-a <busra.abur@gmail.com>
2021-12-10 11:41:45 +0100

    requete favoris

--- Modules/Membre/modele_membre.php
+++ Modules/Membre/modele_membre.php
@@ -6,21 +6,21 @@ require_once "./connexion.php";
         public function __construct() {}
 
         function getForumFav($id) {
-            $req = self::$bdd->prepare("SELECT * FROM FORUMFAV WHERE idUtilisateur = $id;");
+            $req = self::$bdd->prepare("SELECT * FROM Forum NATURAL JOIN FORUMFAV WHERE idUtilisateur = $id;");
             $req->execute();
             $res = $req->fetchAll();
             return $res;
         }
 
         function getArticleFav($id) {
-            $req = self::$bdd->prepare("SELECT * FROM ARTICLEFAV WHERE idUtilisateur = $id;");
+            $req = self::$bdd->prepare("SELECT * FROM Article NATURAL JOIN ARTICLEFAV WHERE idUtilisateur = $id;");
             $req->execute();
             $res = $req->fetchAll();
             return $res;
         }
 
         function getCoursFav($id) {
-            $req = self::$bdd->prepare("SELECT * FROM COURSFAV WHERE idUtilisateur = $id;");
+            $req = self::$bdd->prepare("SELECT * FROM Cours NATURAL JOIN COURSFAV WHERE idUtilisateur = $id;");
             $req->execute();
             $res = $req->fetchAll();
             return $res;
