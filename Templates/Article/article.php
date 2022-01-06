d898c38d43b7f763413c4e7292b307d1e1fb5619
Aslan-06 <aslan.vichegourov@gmail.com>
2022-01-06 11:36:25 +0100

    recupere les tuples de articles et ses attributs

--- Templates/Article/article.php
+++ Templates/Article/article.php
@@ -1,2 +1,20 @@
 <?php
+    echo $_SESSION['articles'][0]['titre'];
+    for($articleCompteur=0; $articleCompteur < count($_SESSION['articles']); $articleCompteur++){
+        //chaque tuple
+        $article = $_SESSION['articles'][$articleCompteur];
+        
+        //tous les attributs de tuple courrant
+        $idArticle = $article['idArticle'];
+        $titreArticle = $article['titre'];
+        $idUtilisateur = $article['idUtilisateur'];
+        $nbVues = $article['nbVues'];
+        $likes = $article['likes'];
+        $dateCreaArticle = $article['dateCreaArticle'];
+        $contenuArticle = $article['contenuArticle'];
+
+        echo $nbVues;
+        echo $likes;
+        echo $contenuArticle;
+    }
 ?>
